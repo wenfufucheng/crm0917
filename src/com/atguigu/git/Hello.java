de23b08b9be98b6ea984f983c2566e6bf237b065
lyw <wenge3529@163.com>
2016-02-24 21:18:33 +0800

    新建java文件

---
+++ src/com/atguigu/git/Hello.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.atguigu.git;
+
+/**
+ * @author wen
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("lallala");
+	}
+
+}
